d392771b9089988da4c9e90c77bea808aa7f77e7
keisuke <57240541+keisukeh1016@users.noreply.github.com>
2023-11-12 05:23:52 +0900

    「四半期累計業績」用プロパティの追加

--- Models/Stock.cs
+++ Models/Stock.cs
@@ -14,38 +14,15 @@ public class Stock
     public string Name { get; set; }
     public Constants.Market Market { get; set; }
     public int Quarter { get; set; }
-    public StockPerformance YearPerformance { get; set; } = new StockPerformance();
-    public StockPerformance YearPerformance1TimesBefore { get; set; } = new StockPerformance();
     public StockPerformance QuarterPerformance { get; set; } = new StockPerformance();
     public StockPerformance QuarterPerformance1TimesBefore { get; set; } = new StockPerformance();
     public StockPerformance QuarterPerformance2TimesBefore { get; set; } = new StockPerformance();
     public StockPerformance QuarterPerformance3TimesBefore { get; set; } = new StockPerformance();
     public StockPerformance QuarterPerformance4TimesBefore { get; set; } = new StockPerformance();
-
-    public decimal? YoyTotalNetSales
-    {
-        get => Rate(YearPerformance.NetSales, YearPerformance1TimesBefore.NetSales);
-    }
-
-    public decimal? YoyTotalOperatingProfit
-    {
-        get => Rate(YearPerformance.OperatingProfit, YearPerformance1TimesBefore.OperatingProfit);
-    }
-
-    public decimal? YoyTotalOrdinaryProfit
-    {
-        get => Rate(YearPerformance.OrdinaryProfit, YearPerformance1TimesBefore.OrdinaryProfit);
-    }
-
-    public decimal? YoyTotalProfit
-    {
-        get => Rate(YearPerformance.Profit, YearPerformance1TimesBefore.Profit);
-    }
-
-    public decimal? YoyTotalEarningsPerShare
-    {
-        get => Rate(YearPerformance.EarningsPerShare, YearPerformance1TimesBefore.EarningsPerShare);
-    }
+    public StockPerformance YtdPerformance { get; set; } = new StockPerformance();
+    public StockPerformance YtdPerformance1TimesBefore { get; set; } = new StockPerformance();
+    public StockPerformance YearPerformance { get; set; } = new StockPerformance();
+    public StockPerformance YearPerformance1TimesBefore { get; set; } = new StockPerformance();
 
     public decimal? QoqNetSales
     {
@@ -95,6 +72,56 @@ public class Stock
     public decimal? YoyEarningsPerShare
     {
         get => Rate(QuarterPerformance.EarningsPerShare, QuarterPerformance4TimesBefore.EarningsPerShare);
+    }
+
+    public decimal? YtdYoyNetSales
+    {
+        get => Rate(YtdPerformance.NetSales, YtdPerformance1TimesBefore.NetSales);
+    }
+
+    public decimal? YtdYoyOperatingProfit
+    {
+        get => Rate(YtdPerformance.OperatingProfit, YtdPerformance1TimesBefore.OperatingProfit);
+    }
+
+    public decimal? YtdYoyOrdinaryProfit
+    {
+        get => Rate(YtdPerformance.OrdinaryProfit, YtdPerformance1TimesBefore.OrdinaryProfit);
+    }
+
+    public decimal? YtdYoyProfit
+    {
+        get => Rate(YtdPerformance.Profit, YtdPerformance1TimesBefore.Profit);
+    }
+
+    public decimal? YtdYoyEarningsPerShare
+    {
+        get => Rate(YtdPerformance.EarningsPerShare, YtdPerformance1TimesBefore.EarningsPerShare);
+    }
+
+    public decimal? FiscalYoyNetSales
+    {
+        get => Rate(YearPerformance.NetSales, YearPerformance1TimesBefore.NetSales);
+    }
+
+    public decimal? FiscalYoyOperatingProfit
+    {
+        get => Rate(YearPerformance.OperatingProfit, YearPerformance1TimesBefore.OperatingProfit);
+    }
+
+    public decimal? FiscalYoyOrdinaryProfit
+    {
+        get => Rate(YearPerformance.OrdinaryProfit, YearPerformance1TimesBefore.OrdinaryProfit);
+    }
+
+    public decimal? FiscalYoyProfit
+    {
+        get => Rate(YearPerformance.Profit, YearPerformance1TimesBefore.Profit);
+    }
+
+    public decimal? FiscalYoyEarningsPerShare
+    {
+        get => Rate(YearPerformance.EarningsPerShare, YearPerformance1TimesBefore.EarningsPerShare);
     }
 
     private static decimal? Rate(decimal? cur, decimal? pre)
