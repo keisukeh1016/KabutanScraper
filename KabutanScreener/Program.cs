a5f5f8a56b7f4e42c7a4e9505eebbadc051b89b9
keisuke <57240541+keisukeh1016@users.noreply.github.com>
2024-06-16 21:16:37 +0900

    KabutanScreenerプロジェクトの追加、ディレクトリ移動

---
+++ KabutanScreener/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace KabutanScreener
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
