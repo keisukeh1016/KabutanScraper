50d96b6742ac932c141d51383190eeee0d44825e
keisuke <57240541+keisukeh1016@users.noreply.github.com>
2024-06-17 02:00:54 +0900

    KabutanScreenerの処理作成完了

---
+++ KabutanScreener/Controllers/MainController.cs
@@ -0,0 +1,25 @@
+﻿namespace KabutanScreener;
+
+public static class MainController
+{
+    public static async Task Scrape(string inputPath, string outputPath)
+    {
+        //List<string> codes = FileService.ReadCodes(inputPath);
+        //Console.WriteLine($"{codes.Count}件の銘柄情報を入力");
+
+        //List<string> htmlList = await HttpService.GetHtmlList(codes);
+        //htmlList = htmlList
+        //    .Where(html => html != "")
+        //    .ToList();
+
+        //List<Stock?> stocks = await HtmlParseService.ParseHtmlList(htmlList);
+        //stocks = stocks
+        //    .Where(stock => stock != null)
+        //    .ToList();
+
+        //FileService.WriteResults(stocks, outputPath);
+        //Console.WriteLine($"{stocks.Count}件の銘柄情報を出力");
+
+        return;
+    }
+}
--- KabutanScreener/Program.cs
+++ KabutanScreener/Program.cs
@@ -1,10 +1,106 @@
-﻿namespace KabutanScreener
+﻿using System.IO;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Unicode;
+using KabutanScraper;
+
+namespace KabutanScreener
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // 引数0
+            string inputPath = Path.GetFullPath(args[0]);
+
+            // 引数1
+            string outputPath = Path.GetFullPath(args[1]);
+
+            // ファイル読み取り
+            List<Stock>? stocks;
+            using (FileStream fs = File.OpenRead(inputPath))
+            {
+                stocks = JsonSerializer.Deserialize<List<Stock>>(fs);
+            }
+            if (stocks == null)
+            {
+                return;
+            }
+
+            // スクリーニング
+            Func<Stock, bool> operatingProfitIsMax = stock =>
+            {
+                if (stock.YearPerformance.OperatingProfitRate == null)
+                {
+                    return true;
+                }
+
+                List<decimal?> operatingProfits = new List<decimal?> {
+                    stock.YearPerformance.OperatingProfitRate,
+                    stock.YearPerformance1TimesBefore.OperatingProfitRate,
+                    stock.YearPerformance2TimesBefore.OperatingProfitRate,
+                    stock.YearPerformance3TimesBefore.OperatingProfitRate,
+                    stock.YearPerformance4TimesBefore.OperatingProfitRate,
+                };
+
+                decimal? max = operatingProfits.Where(x => x != null).Max();
+                bool isMax = max == stock.YearPerformance.OperatingProfitRate;
+
+                return isMax;
+            };
+
+            Func<Stock, bool> ordinaryProfitIsMax = stock =>
+            {
+                if (stock.YearPerformance.OrdinaryProfitRate == null)
+                {
+                    return true;
+                }
+
+                List<decimal?> ordinaryProfits = new List<decimal?> {
+                    stock.YearPerformance.OrdinaryProfitRate,
+                    stock.YearPerformance1TimesBefore.OrdinaryProfitRate,
+                    stock.YearPerformance2TimesBefore.OrdinaryProfitRate,
+                    stock.YearPerformance3TimesBefore.OrdinaryProfitRate,
+                    stock.YearPerformance4TimesBefore.OrdinaryProfitRate,
+                };
+
+                decimal? max = ordinaryProfits.Where(x => x != null).Max();
+                bool isMax = max == stock.YearPerformance.OrdinaryProfitRate;
+
+                return isMax;
+            };
+
+            Func<Stock, bool> profitIsMax = stock =>
+            {
+                if (stock.YearPerformance.ProfitRate == null)
+                {
+                    return true;
+                }
+
+                List<decimal?> profits = new List<decimal?> {
+                    stock.YearPerformance.ProfitRate,
+                    stock.YearPerformance1TimesBefore.ProfitRate,
+                    stock.YearPerformance2TimesBefore.ProfitRate,
+                    stock.YearPerformance3TimesBefore.ProfitRate,
+                    stock.YearPerformance4TimesBefore.ProfitRate,
+                };
+
+                decimal? max = profits.Where(x => x != null).Max();
+                bool isMax = max == stock.YearPerformance.ProfitRate;
+
+                return isMax;
+            };
+
+            List<Stock> screened = stocks
+                .Where(x => operatingProfitIsMax(x) || ordinaryProfitIsMax(x) || profitIsMax(x))
+                .ToList();
+
+            // ファイル出力
+            var options = new JsonSerializerOptions { Encoder = JavaScriptEncoder.Create(UnicodeRanges.All), WriteIndented = true };
+            string jsonString = JsonSerializer.Serialize(screened, options);
+            File.WriteAllText(outputPath, jsonString);
+
+            return;
         }
     }
 }
